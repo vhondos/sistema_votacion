2ea7aac4aa1e58dc8aaa07f8d8b1a2a92a2c751f
FabrizioGabrielCB <devfabo2004@gmail.com>
2024-11-13 16:43:41 +0100

    html, css, ts-base

---
+++ src/ts/main.ts
@@ -0,0 +1,31 @@
+interface Movie {
+    name:string;
+    votos:number;
+}
+
+const PELICULAS:Movie[] =  [
+    {
+        name: "Inside Out 2",
+        votos: 0,
+    },
+    {
+        name: "Deadpool & Wolverine",
+        votos: 0,
+    },
+    {
+        name: "Despicable Me 4",
+        votos: 0,
+    },
+    {
+        name: "Godzilla y Kong: el nuevo imperio",
+        votos: 0,
+    },
+    {
+        name: "Alien: Romulus",
+        votos: 0,
+    }
+]
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("La página ha terminado de cargarse.");
+// });
